eef3aec3b59689ec901759c779c15fa560f10026
Andrei-Gabriel Moldovan <123833398+MoldoAndr@users.noreply.github.com>
2024-06-22 19:46:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ if [[ $# -ne 1 ]]; then
     echo "usage: ./build.sh build_log_file"
     exit 1
 else
-    sudo chmod 777 *
     #Dependente necesare
     sudo apt install libudev-dev make php8.1 python3.10 gcc g++ build-essential ca-certificates curl iproute2 cron -y &>>$1
     ((error_less += $?))
@@ -26,7 +25,7 @@ else
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
         sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-    ($isolated)
+    sudo apt-get update &>>$1
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &>>$1
     ((error_less += $?))
     sudo service docker start &>>$1
